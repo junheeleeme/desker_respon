77642f598264c00dc3e0b1d06449eb957b93ef14
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-05-27 02:31:53 +0900

    풋터 진행중

---
+++ script/script.js
@@ -0,0 +1,14 @@
+//Variables
+
+
+
+
+//Functions
+
+
+main_slider = () => {
+    const main_slide_frame = document.querySelectorAll(".slide_frame>li");
+    console.log(main_slide_frame)
+}
+
+main_slider();
