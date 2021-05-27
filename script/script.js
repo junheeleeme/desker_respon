6fd1923d57bf1360fc62c43510be574627b38304
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-05-27 23:50:56 +0900

    slick 적용 완료

--- script/script.js
+++ script/script.js
@@ -6,9 +6,36 @@
 //Functions
 
 
-main_slider = () => {
-    const main_slide_frame = document.querySelectorAll(".slide_frame>li");
-    console.log(main_slide_frame)
-}
 
-main_slider();
+//Event Handler
+
+$(document).ready(function() {
+
+    $('.mainSlider').slick({ //mainSlider
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots : true, 
+        dotsClass : "main-dots", 
+        appendArrows: false,
+        draggable : false
+        });
+
+    $('.storeSlider').slick({ //storeSlider
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots : true, 
+        dotsClass : "store-dots", 
+        appendArrows: false,
+        draggable : false
+    })
+
+});
+
